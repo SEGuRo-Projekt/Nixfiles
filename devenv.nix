a429f4370b84b5a33f21fe76bddefcb7390ca1bf
Philipp Jungkamp <p.jungkamp@gmx.net>
2023-10-13 13:56:38 +0200

    Add nix flake check pre-commit hook

--- devenv.nix
+++ devenv.nix
@@ -12,5 +12,12 @@
   pre-commit.hooks = {
     alejandra.enable = true;
     editorconfig-checker.enable = true;
+    nix-flake-check = {
+      enable = true;
+      name = "nix flake check";
+      entry = "nix flake check --impure --no-build";
+      language = "system";
+      pass_filenames = false;
+    };
   };
 }
