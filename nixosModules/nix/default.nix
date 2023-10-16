e07affaf258313dd9118979606fd0b1bdf2bb3d2
Steffen Vogel <post@steffenvogel.de>
2023-10-16 15:06:20 +0200

    Add nix config for remote builders and substitutors
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

--- nixosModules/nix/default.nix
+++ nixosModules/nix/default.nix
@@ -12,7 +12,38 @@ in {
     registry = mapAttrs (name: value: {flake = value;}) flakes;
 
     # enable the new nix CLI and flakes
-    settings.experimental-features = ["nix-command" "flakes"];
+    settings = {
+      experimental-features = ["nix-command" "flakes"];
+
+      substitutors = [
+        "s3://nixcache?profile=default&scheme=https&endpoint=s4.0l.de"
+      ];
+
+      buildMachines = [
+        {
+          hostName = "oci.0l.de";
+          system = "aarch64-linux";
+          protocol = "ssh-ng";
+
+          # if the builder supports building for multiple architectures,
+          # replace the previous line by, e.g.,
+          # systems = ["x86_64-linux" "aarch64-linux"];
+
+          maxJobs = 4;
+          speedFactor = 2;
+
+          # supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
+          mandatoryFeatures = [];
+        }
+      ];
+    };
+
+    distributedBuilds = true;
+
+    # Optional, useful when the builder has a faster internet connection than yours
+    extraOptions = ''
+      builders-use-substitutes = true
+    '';
 
     # garbage collect the nix store once a week
     gc = {
