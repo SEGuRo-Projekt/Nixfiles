a697376c760846c510a959a84787da6609365f3c
Philipp Jungkamp <p.jungkamp@gmx.net>
2023-10-22 11:33:07 +0200

    Add nixremote user for testing distributed builds

--- nixosConfigurations/remote-store/configuration.nix
+++ nixosConfigurations/remote-store/configuration.nix
@@ -19,9 +19,24 @@
     useDHCP = true;
   };
 
-  nix.extraOptions = ''
-    secret-key-files = /var/lib/keys/cache.key
-  '';
+  users = {
+    groups.nixremote = {};
+    users.nixremote = {
+      isSystemUser = true;
+      useDefaultShell = true;
+      group = "nixremote";
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOdVp2uN/50APLmPWBjdfzkTHzeFbyr7GZYWWaDz1Lrn root@yoga9"
+      ];
+    };
+  };
+
+  nix = {
+    settings.trusted-users = ["nixremote"];
+    extraOptions = ''
+      secret-key-files = /var/lib/keys/cache.key
+    '';
+  };
 
   time.timeZone = "Europe/Berlin";
 
